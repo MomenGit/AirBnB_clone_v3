f6d170c599ecf9cf3c7cc5e93e803e25ac2a915c
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-10-28 00:47:59 +0200

    Add stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,26 @@ from flask import jsonify
 def get_status():
     """Returns a JSON: "status": "OK"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/api/v1/stats")
+def get_stats():
+    """retrieves the number of each objects by type"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    from models import storage
+
+    classes = {"amenities": Amenity,
+               "cities": City,
+               "places": Place,
+               "reviews": Review,
+               "states": State,
+               "users": User}
+    stats_dict = dict()
+
+    for k, v in classes.items():
+        stats_dict[k] = storage.count(v)
+    return jsonify(stats_dict)
