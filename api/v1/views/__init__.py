6fda67b4c990e06d3a89546d341d6b091c85d5f3
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-28 14:39:49 +0200

    Add imports for all views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,3 +5,9 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from .index import *
+from .states import *
+from .cities import *
+from .amenities import *
+from .users import *
+from .places_reviews import *
+from .places_amenities import *
