7f0f54b187fffa838a1ab230e6ebcaca189f862d
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-11-01 06:44:36 +0200

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,6 @@
 """Defines app_views flask blueprint for views package"""
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
 from .index import *
 from .states import *
 from .cities import *
@@ -12,3 +10,5 @@ from .users import *
 from .places import *
 from .places_reviews import *
 from .places_amenities import *
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
